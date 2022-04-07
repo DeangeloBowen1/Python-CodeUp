1f7ab70cbcc0096b75f1666dcd4e23c2d1c731ef
DeangeloBowen1 <deangelo.bowen15@gmail.com>
2022-04-07 14:39:05 -0500

    python exercises

---
+++ Python Exercises/python_introduction_exercises.py
@@ -0,0 +1,16 @@
+'''
+DeAngelo Bowen
+April 7th, 2022
+Codeup LLC
+Introduction to Python
+'''
+
+check = input('put something here').upper()
+
+
+print(check)
+print('Hello World')  # prints
+
+# Using variables to print 
+greeting = "We will soon begin working with python. Get ready!"
+print(greeting)
